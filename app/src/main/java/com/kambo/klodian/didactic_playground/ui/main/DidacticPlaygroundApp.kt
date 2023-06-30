352aca7c212323d1933f184572a49f020fb7037b
Klodian Kambo <kkambo@technogym.com>
2023-06-30 09:31:23 +0200

    Add App class

---
+++ app/src/main/java/com/kambo/klodian/didactic_playground/ui/main/DidacticPlaygroundApp.kt
@@ -0,0 +1,10 @@
+package com.kambo.klodian.didactic_playground.ui.main
+
+import android.app.Application
+
+class DidacticPlaygroundApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}

3371e67845a21f1badf08ae2b2e4738403783382
Klodian Kambo <klodian.kambo@gmail.com>
2023-07-09 10:16:07 +0200

    feat: dynamic generation of children

--- app/src/main/java/com/kambo/klodian/didactic_playground/ui/main/MainFragment.kt
+++ app/src/main/java/com/kambo/klodian/didactic_playground/ui/main/MainFragment.kt
@@ -1,12 +1,16 @@
 package com.kambo.klodian.didactic_playground.ui.main
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import com.kambo.klodian.didactic_playground.R
+import com.kambo.klodian.didactic_playground.databinding.ChildFormLayoutBinding
+import com.kambo.klodian.didactic_playground.databinding.FragmentMainBinding
 
 /**
  *
@@ -23,6 +27,7 @@ class MainFragment : Fragment() {
 
     // provides the ui data
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +38,33 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.bind(rootView)
+
+        binding.btnGenerate.setOnClickListener {
+            val numChildren = binding.etNumChildren.text.toString().toIntOrNull() ?: 0
+            setChildForm(numChildren)
+            hideKeyboard()
+        }
+
+        return rootView
     }
 
+    private fun setChildForm(numChildren: Int) {
+
+        binding.childFormsContainer.removeAllViews()
+
+        for (i in 1..numChildren) {
+            val childFormView = layoutInflater.inflate(R.layout.child_form_layout, binding.childFormsContainer, false)
+            val childFormLayoutBinding = ChildFormLayoutBinding.bind(childFormView)
+            childFormLayoutBinding.tvChildNumber.text = getString(R.string.child_number, i)
+
+            binding.childFormsContainer.addView(childFormView)
+        }
+    }
+
+    private fun hideKeyboard() {
+        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.windowToken, 0)
+    }
 }

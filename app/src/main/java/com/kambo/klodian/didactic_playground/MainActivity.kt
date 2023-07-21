78d4f916665cc801917a5ed942afe304644ef9b5
Klodian Kambo <klodian.kambo@gmail.com>
2023-07-21 11:20:48 +0200

    Add image saver and picker intents

--- app/src/main/java/com/kambo/klodian/didactic_playground/MainActivity.kt
+++ app/src/main/java/com/kambo/klodian/didactic_playground/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.kambo.klodian.didactic_playground
 
 import android.app.Activity
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -23,6 +28,39 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var launcher: ActivityResultLauncher<Intent>
+
+    private val pickImageActivityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data
+                val selectedImageUri = intent?.data
+                binding.selectedImage.setImageURI(selectedImageUri)
+            }
+        }
+
+    private val saveImageActivityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data
+                val uri = intent?.data
+                uri?.let {
+                    // Grant URI permission to other apps to access the saved image
+                    grantUriPermission(
+                        packageName,
+                        uri,
+                        Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                    )
+
+                    contentResolver.openOutputStream(it)?.use { outputStream ->
+                        // Read the image from res/raw and write it to the MediaStore
+                        resources.openRawResource(R.raw.android_robot).use { inputStream ->
+                            inputStream.copyTo(outputStream)
+                        }
+                    }
+                }
+            }
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,5 +91,25 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SecondActivity::class.java)
             launcher.launch(intent)
         }
+
+
+        binding.selectImageButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            pickImageActivityResult.launch(intent)
+        }
+
+        binding.saveImageButton.setOnClickListener {
+            saveImageToMediaStore()
+        }
+    }
+
+    private fun saveImageToMediaStore() {
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/png"
+            putExtra(Intent.EXTRA_TITLE, "android_robot.png")
+        }
+
+        saveImageActivityResult.launch(intent)
     }
 }

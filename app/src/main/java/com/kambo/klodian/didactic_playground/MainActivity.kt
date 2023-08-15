2fa92c6b3948c64b1b505e1b9cd86bedce3d0de5
Klodian Kambo <klodian.kambo@gmail.com>
2023-08-15 12:37:04 +0200

    Demo fragment a to fragment b

--- app/src/main/java/com/kambo/klodian/didactic_playground/MainActivity.kt
+++ app/src/main/java/com/kambo/klodian/didactic_playground/MainActivity.kt
@@ -2,6 +2,9 @@ package com.kambo.klodian.didactic_playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.kambo.klodian.didactic_playground.databinding.ActivityMainBinding
+import com.kambo.klodian.didactic_playground.ui.main.BFragment
 import com.kambo.klodian.didactic_playground.ui.main.MainFragment
 
 
@@ -16,11 +19,33 @@ class MainActivity : AppCompatActivity() {
     /**
      * [Lifecycle](https://developer.android.com/guide/components/activities/activity-lifecycle#alc)
      */
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this),null,false)
+
         // sets the layout to be rendered for this activity
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener { it ->
+
+            when(it.itemId){
+                R.id.action_a -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, MainFragment.newInstance())
+                        .commitNow()
+                }
+
+                R.id.action_b -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, BFragment())
+                        .commitNow()
+                }
+            }
+            true
+        }
 
         // this statement provides a guarantee to not replace the fragment on rotation
         if (savedInstanceState == null) {
--- app/src/main/java/com/kambo/klodian/didactic_playground/ui/main/MainFragment.kt
+++ app/src/main/java/com/kambo/klodian/didactic_playground/ui/main/MainFragment.kt
@@ -3,10 +3,9 @@ package com.kambo.klodian.didactic_playground.ui.main
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.kambo.klodian.didactic_playground.R
+import com.kambo.klodian.didactic_playground.databinding.FragmentMainBinding
 
 /**
  *
@@ -15,7 +14,7 @@ import com.kambo.klodian.didactic_playground.R
  * It' can't exist outside of an activity, and it depends on that.
  * Has his own life cycle [Fragment lifecycle](https://developer.android.com/guide/fragments/lifecycle)
  */
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.fragment_main) {
 
     companion object {
         fun newInstance() = MainFragment()
@@ -23,17 +22,18 @@ class MainFragment : Fragment() {
 
     // provides the ui data
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding : FragmentMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMainBinding.bind(view)
     }
 
 }
+
+class BFragment : Fragment(R.layout.fragment_b)
